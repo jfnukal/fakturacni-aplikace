5663a06c76927f17f250199a60894cb629a81938
jfnukal <33260274+jfnukal@users.noreply.github.com>
2025-07-24 14:25:12 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAyofRBAZimtTxZFZrl4NVL1Cm5hOkF8D8",
-  authDomain: "ucetnictvi-5418d.firebaseapp.com",
-  projectId: "ucetnictvi-5418d",
-  storageBucket: "ucetnictvi-5418d.appspot.com",
-  messagingSenderId: "354571663683",
-  appId: "1:354571663683:web:f19e21408fd4781b4217f6",
-  measurementId: "G-7NZMSVGHTW"
+  apiKey: 'AIzaSyAyofRBAZimtTxZFZrl4NVL1Cm5hOkF8D8',
+  authDomain: 'ucetnictvi-5418d.firebaseapp.com',
+  projectId: 'ucetnictvi-5418d',
+  storageBucket: 'ucetnictvi-5418d.appspot.com',
+  messagingSenderId: '354571663683',
+  appId: '1:354571663683:web:f19e21408fd4781b4217f6',
+  measurementId: 'G-7NZMSVGHTW',
 };
 
 // Inicializujeme aplikaci moderním způsobem
@@ -20,3 +20,9 @@ const app = initializeApp(firebaseConfig);
 // A exportujeme služby získané z moderních funkcí
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+if (typeof window !== 'undefined') {
+  window.firebaseStorage = storage;
+  window.firebaseApp = app;
+  console.log('🔥 Firebase Storage připojen k window');
+}
