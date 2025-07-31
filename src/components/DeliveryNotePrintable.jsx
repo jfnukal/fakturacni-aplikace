02a9d520cf80fd735a66e6fa0a7431e6ce0426fa
jfnukal <33260274+jfnukal@users.noreply.github.com>
2025-07-31 15:58:19 +0200

    Update DeliveryNotePrintable.jsx

--- src/components/DeliveryNotePrintable.jsx
+++ src/components/DeliveryNotePrintable.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 const DeliveryNotePrintable = React.forwardRef(
   ({ note, supplier, showPrices, vatSettings }, ref) => {
+    if (!note || !supplier || !vatSettings) {
+      return null;
+    }
+
     // Funkce pro formátování data do českého formátu
     const formatDate = (dateString) => {
       if (!dateString) return '';
@@ -35,7 +39,7 @@ const DeliveryNotePrintable = React.forwardRef(
       <div
         ref={ref}
         style={{
-          width: '190mm', // Menší celková šířka
+          width: '190mm',
           padding: '5mm',
           margin: '0',
           fontFamily: 'Arial, sans-serif',
@@ -53,28 +57,48 @@ const DeliveryNotePrintable = React.forwardRef(
             borderCollapse: 'collapse',
           }}
         >
-          <tr>
-            <td style={{ width: '60%', verticalAlign: 'top' }}>
-              <h1
-                style={{
-                  fontSize: '20pt',
-                  fontWeight: 'bold',
-                  margin: '0 0 2mm 0',
-                  color: '#000',
-                }}
-              >
-                Dodací list
-              </h1>
-              <div style={{ fontSize: '11pt' }}>Číslo: {note.number}</div>
-            </td>
-            <td
-              style={{ width: '40%', textAlign: 'right', verticalAlign: 'top' }}
-            >
-              <div style={{ fontSize: '9pt' }}>
-                <strong>Datum vystavení:</strong> {formatDate(note.date)}
-              </div>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td style={{ width: '20%', verticalAlign: 'top' }}>
+                {supplier.logoUrl && (
+                  <div style={{ width: '20mm', height: '20mm', textAlign: 'center' }}>
+                    <img
+                      src={supplier.logoUrl}
+                      alt="Logo"
+                      style={{
+                        maxWidth: '100%',
+                        maxHeight: '100%',
+                        objectFit: 'contain'
+                      }}
+                      crossOrigin="anonymous"
+                    />
+                  </div>
+                )}
+              </td>
+              <td style={{ width: '50%', verticalAlign: 'top' }}>
+                <h1
+                  style={{
+                    fontSize: '20pt',
+                    fontWeight: 'bold',
+                    margin: '0 0 2mm 0',
+                    color: '#000',
+                  }}
+                >
+                  Dodací list
+                </h1>
+                <div style={{ fontSize: '11pt' }}>
+                  Číslo: {note.number}
+                </div>
+              </td>
+              <td style={{ width: '30%', textAlign: 'right', verticalAlign: 'top' }}>
+                <div style={{ fontSize: '9pt' }}>
+                  <div style={{ marginBottom: '1mm' }}>
+                    <strong>Datum vystavení:</strong> {formatDate(note.date)}
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
         </table>
 
         {/* Dodavatel a Odběratel jako tabulka */}
@@ -85,85 +109,89 @@ const DeliveryNotePrintable = React.forwardRef(
             borderCollapse: 'collapse',
           }}
         >
-          <tr>
-            <td
-              style={{
-                width: '50%',
-                verticalAlign: 'top',
-                paddingRight: '5mm',
-              }}
-            >
-              <div
+          <tbody>
+            <tr>
+              <td
                 style={{
-                  fontSize: '7pt',
-                  color: '#666',
-                  marginBottom: '2mm',
-                  fontWeight: 'bold',
-                  letterSpacing: '0.5px',
-                  textTransform: 'uppercase',
+                  width: '50%',
+                  verticalAlign: 'top',
+                  paddingRight: '5mm',
                 }}
               >
-                DODAVATEL
-              </div>
-              <div style={{ fontSize: '9pt', lineHeight: '1.3' }}>
-                <div style={{ fontWeight: 'bold', marginBottom: '1mm' }}>
-                  {supplier.name}
+                <div
+                  style={{
+                    fontSize: '7pt',
+                    color: '#666',
+                    marginBottom: '2mm',
+                    fontWeight: 'bold',
+                    letterSpacing: '0.5px',
+                    textTransform: 'uppercase',
+                  }}
+                >
+                  DODAVATEL
                 </div>
-                <div style={{ marginBottom: '1mm' }}>{supplier.address}</div>
-                <div style={{ marginBottom: '2mm' }}>
-                  {supplier.zip} {supplier.city}
-                </div>
-                <div style={{ fontSize: '8pt' }}>
-                  <div style={{ marginBottom: '1mm' }}>IČO: {supplier.ico}</div>
-                  {supplier.dic && (
-                    <div style={{ marginBottom: '1mm' }}>
-                      DIČ: {supplier.dic}
-                    </div>
-                  )}
-                  {vatSettings && !vatSettings.enabled && (
-                    <div style={{ fontSize: '8pt', fontWeight: 'bold' }}>
-                      Neplátce DPH
-                    </div>
-                  )}
-                </div>
-              </div>
-            </td>
-            <td style={{ width: '50%', verticalAlign: 'top' }}>
-              <div
-                style={{
-                  fontSize: '7pt',
-                  color: '#666',
-                  marginBottom: '2mm',
-                  fontWeight: 'bold',
-                  letterSpacing: '0.5px',
-                  textTransform: 'uppercase',
-                }}
-              >
-                ODBĚRATEL
-              </div>
-              <div style={{ fontSize: '9pt', lineHeight: '1.3' }}>
-                <div style={{ fontWeight: 'bold', marginBottom: '1mm' }}>
-                  {note.customer?.name}
-                </div>
-                <div style={{ marginBottom: '1mm' }}>
-                  {note.customer?.address}
-                </div>
-                <div style={{ marginBottom: '2mm' }}>
-                  {note.customer?.zip} {note.customer?.city}
-                </div>
-                <div style={{ fontSize: '8pt' }}>
-                  <div style={{ marginBottom: '1mm' }}>
-                    IČO: {note.customer?.ico}
+                <div style={{ fontSize: '9pt', lineHeight: '1.3' }}>
+                  <div style={{ fontWeight: 'bold', marginBottom: '1mm' }}>
+                    {supplier.name}
                   </div>
-                  {note.customer?.dic && (
+                  <div style={{ marginBottom: '1mm' }}>{supplier.address}</div>
+                  <div style={{ marginBottom: '2mm' }}>
+                    {supplier.zip} {supplier.city}
+                  </div>
+                  <div style={{ fontSize: '8pt' }}>
                     <div style={{ marginBottom: '1mm' }}>
-                      DIČ: {note.customer?.dic}
+                      IČO: {supplier.ico}
                     </div>
-                  )}
+                    {supplier.dic && (
+                      <div style={{ marginBottom: '1mm' }}>
+                        DIČ: {supplier.dic}
+                      </div>
+                    )}
+                    {vatSettings && !vatSettings.enabled && (
+                      <div style={{ fontSize: '8pt', fontWeight: 'bold' }}>
+                        Neplátce DPH
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </td>
-          </tr>
+              </td>
+              <td style={{ width: '50%', verticalAlign: 'top' }}>
+                <div
+                  style={{
+                    fontSize: '7pt',
+                    color: '#666',
+                    marginBottom: '2mm',
+                    fontWeight: 'bold',
+                    letterSpacing: '0.5px',
+                    textTransform: 'uppercase',
+                  }}
+                >
+                  ODBĚRATEL
+                </div>
+                <div style={{ fontSize: '9pt', lineHeight: '1.3' }}>
+                  <div style={{ fontWeight: 'bold', marginBottom: '1mm' }}>
+                    {note.customer?.name}
+                  </div>
+                  <div style={{ marginBottom: '1mm' }}>
+                    {note.customer?.address}
+                  </div>
+                  <div style={{ marginBottom: '2mm' }}>
+                    {note.customer?.zip} {note.customer?.city}
+                  </div>
+                  <div style={{ fontSize: '8pt' }}>
+                    <div style={{ marginBottom: '1mm' }}>
+                      IČO: {note.customer?.ico}
+                    </div>
+                    {note.customer?.dic && (
+                      <div style={{ marginBottom: '1mm' }}>
+                        DIČ: {note.customer?.dic}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
         </table>
 
         {/* Tabulka položek */}
@@ -311,110 +339,105 @@ const DeliveryNotePrintable = React.forwardRef(
         {/* Celková částka jako tabulka */}
         {showPrices && (
           <table style={{ width: '100%', marginBottom: '8mm' }}>
-            <tr>
-              <td style={{ width: '60%' }}></td>
-              <td style={{ width: '40%' }}>
-                <table style={{ width: '100%', fontSize: '9pt' }}>
-                  <tr>
-                    <td
-                      style={{
-                        padding: '1mm 0',
-                        borderTop: '1px solid #000',
-                        fontWeight: 'bold',
-                      }}
-                    >
-                      Celkem bez DPH:
-                    </td>
-                    <td
-                      style={{
-                        textAlign: 'right',
-                        padding: '1mm 0',
-                        borderTop: '1px solid #000',
-                        fontWeight: 'bold',
-                      }}
-                    >
-                      {total.toFixed(2)} Kč
-                    </td>
-                  </tr>
-                  {vatSettings?.enabled && (
-                    <>
+            <tbody>
+              <tr>
+                <td style={{ width: '40%', verticalAlign: 'top' }}>
+                  {/* Prázdné místo pro eventuální QR kód nebo jiný obsah */}
+                </td>
+                <td style={{ width: '60%', verticalAlign: 'top' }}>
+                  <table style={{ width: '100%', fontSize: '9pt', marginLeft: 'auto', maxWidth: '60%' }}>
+                    <tbody>
                       <tr>
                         <td style={{ padding: '0.5mm 0' }}>
-                          DPH {vatSettings.rate}%:
+                          Celkem bez DPH:
                         </td>
                         <td style={{ textAlign: 'right', padding: '0.5mm 0' }}>
-                          {(total * (vatSettings.rate / 100)).toFixed(2)} Kč
+                          {total.toFixed(2)} Kč
                         </td>
                       </tr>
-                      <tr>
-                        <td
-                          style={{
-                            padding: '1mm 0',
-                            borderTop: '1px solid #000',
-                            fontWeight: 'bold',
-                            fontSize: '10pt',
-                          }}
-                        >
-                          Celkem s DPH:
-                        </td>
-                        <td
-                          style={{
-                            textAlign: 'right',
-                            padding: '1mm 0',
-                            borderTop: '1px solid #000',
-                            fontWeight: 'bold',
-                            fontSize: '10pt',
-                          }}
-                        >
-                          {(total * (1 + vatSettings.rate / 100)).toFixed(2)} Kč
-                        </td>
-                      </tr>
-                    </>
-                  )}
-                </table>
-              </td>
-            </tr>
+                      {vatSettings?.enabled && (
+                        <>
+                          <tr>
+                            <td style={{ padding: '0.5mm 0' }}>
+                              DPH {vatSettings.rate}%:
+                            </td>
+                            <td style={{ textAlign: 'right', padding: '0.5mm 0' }}>
+                              {(total * (vatSettings.rate / 100)).toFixed(2)} Kč
+                            </td>
+                          </tr>
+                          <tr>
+                            <td
+                              style={{
+                                padding: '1mm 0',
+                                borderTop: '1px solid #000',
+                                fontWeight: 'bold',
+                                fontSize: '10pt',
+                              }}
+                            >
+                              Celkem s DPH:
+                            </td>
+                            <td
+                              style={{
+                                textAlign: 'right',
+                                padding: '1mm 0',
+                                borderTop: '1px solid #000',
+                                fontWeight: 'bold',
+                                fontSize: '10pt',
+                              }}
+                            >
+                              {(total * (1 + vatSettings.rate / 100)).toFixed(2)} Kč
+                            </td>
+                          </tr>
+                        </>
+                      )}
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
           </table>
         )}
 
         {/* Podpisy jako tabulka */}
         <table style={{ width: '100%', marginTop: '15mm' }}>
-          <tr>
-            <td
-              style={{
-                width: '50%',
-                textAlign: 'center',
-                paddingRight: '10mm',
-              }}
-            >
-              <div
+          <tbody>
+            <tr>
+              <td
                 style={{
-                  borderTop: '1px solid #000',
-                  paddingTop: '3mm',
-                  marginTop: '10mm',
+                  width: '50%',
+                  textAlign: 'center',
+                  paddingRight: '10mm',
                 }}
               >
-                <div style={{ fontSize: '8pt', fontWeight: 'bold' }}>
-                  Podpis dodavatele
+                <div
+                  style={{
+                    borderTop: '1px solid #000',
+                    paddingTop: '3mm',
+                    marginTop: '10mm',
+                  }}
+                >
+                  <div style={{ fontSize: '8pt', fontWeight: 'bold' }}>
+                    Podpis dodavatele
+                  </div>
                 </div>
-              </div>
-            </td>
-            <td
-              style={{ width: '50%', textAlign: 'center', paddingLeft: '10mm' }}
-            >
-              <div
-                style={{
-                  borderTop: '1px solid #000',
-                  paddingTop: '3mm',
-                  marginTop: '10mm',
-                }}
+              </td>
+              <td
+                style={{ width: '50%', textAlign: 'center', paddingLeft: '10mm' }}
               >
-                <div style={{ fontSize: '8pt', fontWeight: 'bold' }}>
-                  Podpis odběratele
+                <div
+                  style={{
+                    borderTop: '1px solid #000',
+                    paddingTop: '3mm',
+                    marginTop: '10mm',
+                  }}
+                >
+                  <div style={{ fontSize: '8pt', fontWeight: 'bold' }}>
+                    Podpis odběratele
+                  </div>
                 </div>
-              </div>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
